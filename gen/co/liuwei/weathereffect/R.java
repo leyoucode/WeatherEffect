999d76dc14d04cab30d381c936d6e62dbcde24bc
liuwei <i@liuwei.co>
2014-04-02 17:07:49 +0800

    Snow effect,Cloudy effect,Windy effect

--- gen/co/liuwei/weathereffect/R.java
+++ gen/co/liuwei/weathereffect/R.java
@@ -21,10 +21,29 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int bg14_day_snow=0x7f020000;
+        public static final int bg_na=0x7f020001;
+        public static final int bg_rain_night=0x7f020002;
+        public static final int ic_launcher=0x7f020003;
+        public static final int na_point=0x7f020004;
+        public static final int na_windmill=0x7f020005;
+        public static final int snowflake_l=0x7f020006;
+        public static final int snowflake_m=0x7f020007;
+        public static final int snowflake_s=0x7f020008;
+        public static final int snowflake_xl=0x7f020009;
+        public static final int snowflake_xxl=0x7f02000a;
+        public static final int yjjc_h_a1=0x7f02000b;
+        public static final int yjjc_h_a2=0x7f02000c;
+        public static final int yjjc_h_a3=0x7f02000d;
+        public static final int yjjc_h_a4=0x7f02000e;
+        public static final int yjjc_h_a5=0x7f02000f;
+        public static final int yjjc_h_a6=0x7f020010;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f080003;
+        public static final int linear1=0x7f080001;
+        public static final int linear2=0x7f080002;
+        public static final int linearWeatherBg=0x7f080000;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
--- src/co/liuwei/weathereffect/MainActivity.java
+++ src/co/liuwei/weathereffect/MainActivity.java
@@ -1,22 +1,71 @@
 package co.liuwei.weathereffect;
 
-import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
+import co.liuwei.weathereffect.view.DynamicWeatherCloudyView;
+import co.liuwei.weathereffect.view.LoadWeatherView;
+import co.liuwei.weathereffect.view.SnowSurfaceView;
 
 public class MainActivity extends Activity {
 
+	private FrameLayout linearWeatherBg;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		//±³¾°ÈÝÆ÷
+		linearWeatherBg = (FrameLayout)findViewById(R.id.linearWeatherBg);
+		
+		
+		
+		
+		
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
+	public void doRain(View v){
+		
+	}
+	public void doSnow(View v){
+		linearWeatherBg.removeAllViews();
+		SnowSurfaceView snowSurfaceView=new SnowSurfaceView(this);
+		linearWeatherBg.addView(snowSurfaceView);
+	}
+	public void doOvercast(View v){
+		
+	}
+	public void doCloudy(View v){
+		linearWeatherBg.removeAllViews();
+		DynamicWeatherCloudyView view1=new DynamicWeatherCloudyView(this,
+				 R.drawable.yjjc_h_a2, -150, 40,20);
+				 DynamicWeatherCloudyView view4=new DynamicWeatherCloudyView(this,
+				 R.drawable.yjjc_h_a5, 0, 60,30);
+				 DynamicWeatherCloudyView view2=new DynamicWeatherCloudyView(this,
+				 R.drawable.yjjc_h_a3, 280, 80,50);
+				 DynamicWeatherCloudyView view3=new DynamicWeatherCloudyView(this,
+				 R.drawable.yjjc_h_a4, 140, 130,40);
+				 linearWeatherBg.addView(view1);
+				 linearWeatherBg.addView(view2);
+				 linearWeatherBg.addView(view3);
+				 linearWeatherBg.addView(view4);
+				 view1.move();
+				 view2.move();
+				 view3.move();
+				 view4.move();
+	}
+	public void doSunny(View v){
+		
+	}
+	public void doFog(View v){
+		
+	}
+	public void doWind(View v){
+		linearWeatherBg.removeAllViews();
+		LoadWeatherView loadWeatherView = new LoadWeatherView(this);
+		linearWeatherBg.addView(loadWeatherView);
 	}
 
 }
